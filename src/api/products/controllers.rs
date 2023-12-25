de0a6c764132dc56d7f15b90c95e1da82a18f24f
Diego Viniegra <diego.viniegra@gmail.com>
2023-12-25 15:14:20 -0500

    Handle errors

--- src/api/products/controllers.rs
+++ src/api/products/controllers.rs
@@ -22,60 +22,109 @@ pub fn list_products(_req: &mut Request, _depot: &mut Depot, res: &mut Response)
 
 #[handler]
 pub async fn add_product(req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-
-    let form_data: &salvo::http::form::FormData = req.form_data()
-        .await
-        .expect("Error getting the form data");
-
-    let new_product = cast_form_data_to_new_product(form_data)
-        .expect("ups");
-
-    let product = repo::insert_product(new_product)
-        .expect("Error inserting product");
-
-    res.render(Json(product))
+    match req.form_data().await {
+        Err(error) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render(format!("Error getting the form data: {error}"));
+        },
+        Ok(form_data) => match cast_form_data_to_new_product(form_data) {
+            Err(error) => {
+                res.status_code(StatusCode::BAD_REQUEST);
+                res.render(format!("Error parsing the form data fields: {error:?}"));
+            },
+            Ok(new_product) => match repo::insert_product(new_product) {
+                Err(error) => {
+                    res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
+                    res.render(format!("Error inserting product: {error}"));
+                },
+                Ok(product) => {
+                    res.render(Json(product));
+                }
+            }
+        }
+    }
 }
 
 #[handler]
 pub fn show_product(req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-    let id: i32 = utils::get_req_param(req, "id")
-        .unwrap_or_default();
-
-    let product = repo::find_product(id)
-        .expect("Product not found");
-
-    res.render(Json(product))
+    match utils::get_req_param::<i32>(req, "id") {
+        Err(error) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render(format!("Incorrect <id>: {error}"));
+        },
+        Ok(id) => match repo::find_product(id) {
+            Err(_) => {
+                res.status_code(StatusCode::NOT_FOUND);
+            },
+            Ok(product) => {
+                res.render(Json(product));
+            }
+        }
+    }
 }
 
 #[handler]
 pub fn remove_product(req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-    let id: i32 = utils::get_req_param(req, "id")
-        .unwrap_or_default();
-    
-    let total_deleted = repo::delete_product(id)
-        .expect("Failed deleting");
-
-    res.render(format!("Total deleted: {}", total_deleted))
+    match utils::get_req_param(req, "id") {
+        Err(error) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render(format!("Incorrect <id>: {error}"));
+        },
+        Ok(id) => match repo::delete_product(id) {
+            Err(error) => {
+                res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
+                res.render(format!("Error deleting `product`: {error}"));
+            },
+            Ok(total_deleted) => match total_deleted {
+                0 => {
+                    res.status_code(StatusCode::NOT_FOUND);
+                    res.render(format!("Nothing was deleted"));
+                },
+                1 => {
+                    res.status_code(StatusCode::ACCEPTED);
+                },
+                _other => {
+                    res.render(format!("Total deleted: {}", total_deleted));
+                }
+            }
+        }
+    }
 }
 
 #[handler]
 pub async fn update_product(req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-    let id: i32 = utils::get_req_param(req, "id")
-        .unwrap_or_default();
+    match utils::get_req_param(req, "id") {
+        Err(error) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render(format!("Incorrect <id>: {error}"));
+        },
+        Ok(id) => match req.form_data().await {
+            Err(error) => {
+                res.status_code(StatusCode::BAD_REQUEST);
+                res.render(format!("Error getting the form data: {error}"));
+            },
+            Ok(form_data) => match repo::find_product(id) {
+                Err(_) => {
+                    res.status_code(StatusCode::NOT_FOUND);
+                    res.render("Error `product` not found");
+                },
+                Ok(product) => {
+                    let product_updated = product.merge(form_data);
 
-    let form_data = req.form_data()
-        .await
-        .expect("Error getting the form data");
-    
-    let product = repo::find_product(id)
-        .expect("Product not found");
-
-    let product_updated = product.merge(form_data);
-
-    repo::update_product(&product_updated)
-        .expect("Error updating");
-
-    res.render(Json(product_updated))
+                    match repo::update_product(&product_updated) {
+                        Err(error) => {
+                            res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
+                            res.render(format!("Error updating `product`: {error}"));
+                        },
+                        Ok(product_updated) => {
+                            res.status_code(StatusCode::ACCEPTED);
+                            res.render(Json(product_updated));
+                        }
+                    }
+                }                
+            }
+        }   
+    }
 }
 
 fn cast_form_data_to_new_product(form_data: &FormData) -> Result<NewProduct, Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use salvo::prelude::*;
 use fishbowl::api::products;
+use dotenvy::dotenv;
+use std::env;
 
 #[handler]
 async fn hello(_req: &mut Request, _depot: &mut Depot, res: &mut Response) {
@@ -9,12 +11,16 @@ async fn hello(_req: &mut Request, _depot: &mut Depot, res: &mut Response) {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let domain = env::var("DOMAIN").expect("`DOMAIN` must be set");
+    let port = env::var("PORT").expect("`PORT` must be set");
+
     tracing_subscriber::fmt().init();
 
     let router = Router::new().get(hello)
         .push(products::get_router());
 
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    let acceptor = TcpListener::new(format!("{domain}:{port}")).bind().await;
 
     Server::new(acceptor).serve(router).await;
 }
