74af38779439a9dd71308f5769f883c9cda4b5cc
Diego Viniegra <diego.viniegra@gmail.com>
2023-12-29 19:18:20 -0500

    Make error response a json

--- src/api/errors.rs
+++ src/api/errors.rs
@@ -1,5 +1,6 @@
 use std::fmt::{Display, Debug};
 use salvo::prelude::*;
+use serde::Serialize;
 
 #[derive(Debug)]
 pub enum Error<'a> {
@@ -7,47 +8,63 @@ pub enum Error<'a> {
     ParseFloatErr(&'a str),
 }
 
+#[derive(Serialize)]
+struct ErrorMessage {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    error: ErrorMessage,
+}
+
+fn make_json_response(message: String) -> Json<ErrorResponse> {
+    Json(ErrorResponse { error: ErrorMessage { message } })
+}
+
+use make_json_response as json;
+
 pub fn render_form_data_error(res: &mut Response, error: impl Display) {
     res.status_code(StatusCode::BAD_REQUEST);
-    res.render(format!("Error getting the form data: {error}"));
+    res.render(json(format!("Error getting the form data: {error}")));
 }
 
 pub fn render_resource_not_found(res: &mut Response, resource: impl Display) {
     res.status_code(StatusCode::NOT_FOUND);
-    res.render(format!("Error `{resource}` not found"));
+    res.render(json(format!("Error `{resource}` not found")));
 }
 
 pub fn render_cast_error(res: &mut Response, error: impl Debug) {
     res.status_code(StatusCode::BAD_REQUEST);
-    res.render(format!("Error parsing the form data fields: {error:?}"));
+    res.render(json(format!("Error parsing the form data fields: {error:?}")));
 }
 
 pub fn render_parse_field_error(res: &mut Response, error: impl Display, field: impl Display) {
     res.status_code(StatusCode::BAD_REQUEST);
-    res.render(format!("Incorrect <{field}>: {error}"));
+    res.render(json(format!("Incorrect <{field}>: {error}")));
 }
 
 pub fn render_get_user_id_not_found(res: &mut Response) {
     res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-    res.render(format!("Couldn't read `user_id` from depot"));
+    res.render(json(format!("Couldn't read `user_id` from depot")));
 }
 
 pub fn render_db_retrieving_error(res: &mut Response, error: impl Display, resource: impl Display) {
     res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-    res.render(format!("Error loading `{resource}`: {error}"));
+    res.render(json(format!("Error loading `{resource}`: {error}")));
 }
 
 pub fn render_db_insert_error(res: &mut Response, error: impl Display, resource: impl Display) {
     res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-    res.render(format!("Error inserting `{resource}`: {error}"));
+    res.render(json(format!("Error inserting `{resource}`: {error}")));
 }
 
 pub fn render_db_update_error(res: &mut Response, error: impl Display, resource: impl Display) {
     res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-    res.render(format!("Error updating `{resource}`: {error}"));
+    res.render(json(format!("Error updating `{resource}`: {error}")));
 }
 
 pub fn render_db_delete_error(res: &mut Response, error: impl Display, resource: impl Display) {
     res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-    res.render(format!("Error deleting `{resource}`: {error}"));
+    res.render(json(format!("Error deleting `{resource}`: {error}")));
 }
