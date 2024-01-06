305b54c423b903dd81e209d6b89138e6fa08b91d
Diego Viniegra <diego.viniegra@gmail.com>
2024-01-06 17:03:20 -0500

    Fix catcher for options

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use salvo::prelude::*;
-use salvo::cors::Cors;
-use salvo::http::Method;
-use fishbowl::home::home_controller;
+use dotenvy::dotenv;
 use fishbowl::api;
 use fishbowl::api::auth;
-use dotenvy::dotenv;
+use fishbowl::home::home_controller;
+use salvo::catcher::Catcher;
+use salvo::cors::Cors;
+use salvo::http::Method;
+use salvo::prelude::*;
 use std::env;
 
 #[tokio::main]
@@ -16,24 +17,25 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     let cors_handler = Cors::new()
-    .allow_origin("http://localhost:5173")
-    .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
-    .into_handler();
+        .allow_origin(vec!["http://localhost:5173"])
+        .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
+        .allow_headers(vec!["content-type", "authorization"])
+        .into_handler();
 
     let router = Router::new()
-        .hoop(cors_handler)
+        .hoop(cors_handler.clone())
         .hoop(auth::decode_token())
         .get(home_controller)
-        .push(Router::with_path("files/<**path>").get(
-            StaticDir::new([
-                env::current_dir().unwrap().join("static")
-                ])
-            .auto_list(true)
-        ))
-        .push(Router::with_path("auth").post(auth::authenticate))
+        .push(
+            Router::with_path("files/<**path>")
+                .get(StaticDir::new([env::current_dir().unwrap().join("static")]).auto_list(true)),
+        )
+        .push(Router::with_path("api/v1/auth").post(auth::authenticate))
         .push(api::get_router());
+
+    let service = Service::new(router).catcher(Catcher::default().hoop(cors_handler));
 
     let acceptor = TcpListener::new(format!("{domain}:{port}")).bind().await;
 
-    Server::new(acceptor).serve(router).await;
+    Server::new(acceptor).serve(service).await;
 }
