06b4d6f9e44f576620ca2daf8def1850aaa15387
Diego Viniegra <diego.viniegra@gmail.com>
2024-01-12 21:08:54 -0500

    Move start_server to lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,15 @@ pub mod schema;
 pub mod api;
 pub mod home;
 
+use dotenvy::dotenv;
+use salvo::prelude::*;
+use api::auth;
+use home::home_controller;
+use salvo::catcher::Catcher;
+use salvo::cors::Cors;
+use salvo::http::Method;
+use std::env;
+
 pub mod db {
     use diesel::pg::PgConnection;
     use diesel::prelude::*;
@@ -46,4 +55,54 @@ pub mod models {
             }
         }
     }
+}
+
+pub struct Config {
+    domain: String,
+    port: String,
+    client_url: String,
+}
+
+impl Config {
+    pub fn build() -> Self {
+        dotenv().ok();
+
+        let domain = env::var("DOMAIN").expect("`DOMAIN` must be set");
+        let port = env::var("PORT").expect("`PORT` must be set");
+        let client_url = env::var("CLIENT_URL").expect("`CLIENT_URL` must be set");
+
+        Self {
+            domain,
+            port,
+            client_url,
+        }
+    }
+}
+
+#[tokio::main]
+pub async fn start_server(config: Config) {
+    let Config { domain, port, client_url } = config;
+
+    let cors_handler = Cors::new()
+        .allow_origin(vec![client_url.as_str()])
+        .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
+        .allow_headers(vec!["content-type", "authorization"])
+        .into_handler();
+
+    let router = Router::new()
+        .hoop(cors_handler.clone())
+        .hoop(auth::decode_token())
+        .get(home_controller)
+        .push(
+            Router::with_path("files/<**path>")
+                .get(StaticDir::new([env::current_dir().unwrap().join("static")]).auto_list(true)),
+        )
+        .push(Router::with_path("api/v1/auth").post(auth::authenticate))
+        .push(api::get_router());
+
+    let service = Service::new(router).catcher(Catcher::default().hoop(cors_handler));
+
+    let acceptor = TcpListener::new(format!("{domain}:{port}")).bind().await;
+
+    Server::new(acceptor).serve(service).await;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-use dotenvy::dotenv;
-use fishbowl::api;
-use fishbowl::api::auth;
-use fishbowl::home::home_controller;
-use salvo::catcher::Catcher;
-use salvo::cors::Cors;
-use salvo::http::Method;
-use salvo::prelude::*;
-use std::env;
+use fishbowl::{start_server, Config};
 
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-    let domain = env::var("DOMAIN").expect("`DOMAIN` must be set");
-    let port = env::var("PORT").expect("`PORT` must be set");
+fn main() {
+    let config = Config::build();
 
-    tracing_subscriber::fmt().init();
-
-    let cors_handler = Cors::new()
-        .allow_origin(vec!["http://localhost:5173"])
-        .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
-        .allow_headers(vec!["content-type", "authorization"])
-        .into_handler();
-
-    let router = Router::new()
-        .hoop(cors_handler.clone())
-        .hoop(auth::decode_token())
-        .get(home_controller)
-        .push(
-            Router::with_path("files/<**path>")
-                .get(StaticDir::new([env::current_dir().unwrap().join("static")]).auto_list(true)),
-        )
-        .push(Router::with_path("api/v1/auth").post(auth::authenticate))
-        .push(api::get_router());
-
-    let service = Service::new(router).catcher(Catcher::default().hoop(cors_handler));
-
-    let acceptor = TcpListener::new(format!("{domain}:{port}")).bind().await;
-
-    Server::new(acceptor).serve(service).await;
+    start_server(config)
 }
